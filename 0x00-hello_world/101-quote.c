77ceea0e841d753598cd826cf14fc655590caeb6
jeanwachira <kimzwachira@gmail.com>
2023-07-17 14:09:44 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print a line of text followed by  a new line to standard e
+ * Return: Alway 1.
+ */
+
+int main(void)
+
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
