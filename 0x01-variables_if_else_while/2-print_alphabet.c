2a10f581b21ebcd1d391c022777efa9c0afa6509
jeanwachira <kimzwachira@gmail.com>
2023-07-17 15:20:10 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alhabet
+ * return: Always
+ *
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
