8ad53130529cd5b7c33ca59b18e499fb463d0e44
jeanwachira <kimzwachira@gmail.com>
2023-07-17 11:37:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - A program that prints a line with puts funtion
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
